cade152a9d04311668bc5c3bed6e03d244f66896
seylu <98249191+seyLu@users.noreply.github.com>
2023-10-02 23:49:32 +0800

    chore: test fix mypy errors

--- app/settings.py
+++ app/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-=*(%1v&2znr5rwj%^y$trovb=$)sxgr^5j2ojnbn#jeyo#bx&1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS: list[str] = []
 
 
 # Application definition
