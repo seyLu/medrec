9a0d4c1f421d323a79b0c0181ae8123eedb26b96
seylu <98249191+seyLu@users.noreply.github.com>
2023-10-03 23:46:10 +0800

    feat: add register and logout logic

--- auth/views.py
+++ auth/views.py
@@ -5,6 +5,8 @@ from django.urls import reverse
 from django.views.generic import View
 from django_htmx.http import HttpResponseClientRedirect
 
+from users.models import User
+
 
 class LoginView(View):
     def get(self, request: HttpRequest) -> HttpResponse:
@@ -16,7 +18,7 @@ class LoginView(View):
 
         user = authenticate(request, email=email, password=password)
         if user is None:
-            return HttpResponse({"detail": "Invalid Email or Password!"}, status=400)
+            return HttpResponse("Invalid Email or Password!")
 
         login(request, user)
         return HttpResponseClientRedirect(reverse("index"))
@@ -25,9 +27,28 @@ class LoginView(View):
 class LogoutView(View):
     def post(self, request: HttpRequest) -> HttpResponse:
         logout(request)
-        return HttpResponse({"message": "Successfully logged out."})
+        return HttpResponseClientRedirect(reverse("login"))
 
 
 class RegisterView(View):
     def get(self, request: HttpRequest) -> HttpResponse:
         return render(request, "auth/register.html")
+
+    def post(self, request: HttpRequest) -> HttpResponse:
+        email: str = request.POST["email"].strip()
+        password: str = request.POST["password"]
+        re_password: str = request.POST["re-password"]
+
+        if User.objects.filter(email=email).exists():
+            return HttpResponse("User already exists.")
+
+        if password != re_password:
+            return HttpResponse("Password and Confirm Password do not match.")
+
+        user = authenticate(request, email=email, password=password)
+        if user:
+            return HttpResponse({"detail": "User already exists!"}, status=400)
+
+        user = User.objects.create_user(email, password)
+        login(request, user)
+        return HttpResponseClientRedirect(reverse("index"))
