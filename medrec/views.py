572206c87a3279d62c306639661823d6c74c452d
seylu <98249191+seyLu@users.noreply.github.com>
2023-10-03 13:26:15 +0800

    bugfix: fix missing request type

--- medrec/views.py
+++ medrec/views.py
@@ -1,8 +1,8 @@
-from django.http import HttpResponse
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 from django.views.generic import View
 
 
 class IndexView(View):
-    def get(self, request, *args, **kwargs) -> HttpResponse:
+    def get(self, request: HttpRequest) -> HttpResponse:
         return render(request, "medrec/index.html")
