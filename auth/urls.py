b56f7ac13cf9f34076f320ed6180f25a1d431d41
セイル · MJ Sabit <98249191+seyLu@users.noreply.github.com>
2023-10-04 13:11:06 +0800

    [MEDREC-2] Add daisyui (#7)
    * feat: add daisyui
    
    * docs: update stack
    
    * feat: use daisyui on login and register form
    
    * feat: add alpine.js
    
    * feat: add alpine toggle student and employee search
    
    * bugfix: fix nav alignment
    
    * bugfix: fix scroll overflow

--- auth/urls.py
+++ auth/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path("login/", views.LoginView.as_view(), name="login"),
-    path("auth/logout/", views.LogoutView.as_view(), name="logout"),
+    path("logout/", views.LogoutView.as_view(), name="logout"),
     path("register/", views.RegisterView.as_view(), name="register"),
 ]
