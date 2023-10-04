c5abc19c41031cbf92b1d21a463c461131c17b8a
seylu <98249191+seyLu@users.noreply.github.com>
2023-10-04 23:07:38 +0800

    bugfix: fix mypy errors

--- regions/urls.py
+++ regions/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path("districts/", views.DistrictsQueryView.as_view(), name="districts-query"),
+    path("cities/", views.CitiesQueryView.as_view(), name="cities-query"),
 ]
--- regions/views.py
+++ regions/views.py
@@ -1,10 +1,9 @@
-from typing import Any
 from urllib.parse import parse_qs, urlparse
 
 from django.http import HttpRequest, HttpResponse
 from django.views.generic import View
 
-from .models import District
+from .models import City, District
 
 
 class DistrictsQueryView(View):
@@ -12,7 +11,7 @@ class DistrictsQueryView(View):
         parsed_url = urlparse(request.get_full_path())
         regions: dict[str, list[str]] = parse_qs(parsed_url.query)
 
-        response: list[dict[str, Any]] = []
+        response = None
 
         if code := regions.get("code"):
             code = code[0]  # type: ignore
@@ -20,13 +19,34 @@ class DistrictsQueryView(View):
 
         elif city_code := regions.get("city_code"):
             city_code = city_code[0]  # type: ignore
-            response = District.objects.filter(city_code=city_code)
+            response = District.objects.filter(city_code=city_code)  # type: ignore
 
         elif province_code := regions.get("province_code"):
             province_code = province_code[0]  # type: ignore
-            response = District.objects.filter(province_code=province_code)[:10]
+            response = District.objects.filter(province_code=province_code)[:10]  # type: ignore
 
         else:
-            response = District.objects.all()[:10]
+            response = District.objects.all()[:10]  # type: ignore
+
+        return HttpResponse(response)
+
+
+class CitiesQueryView(View):
+    def get(self, request: HttpRequest) -> HttpResponse:
+        parsed_url = urlparse(request.get_full_path())
+        regions: dict[str, list[str]] = parse_qs(parsed_url.query)
+
+        response = None
+
+        if code := regions.get("code"):
+            code = code[0]  # type: ignore
+            response = City.objects.get(code=code)
+
+        elif province_code := regions.get("province_code"):
+            province_code = province_code[0]  # type: ignore
+            response = City.objects.filter(province_code=province_code)  # type: ignore
+
+        else:
+            response = City.objects.all()[:10]  # type: ignore
 
         return HttpResponse(response)
