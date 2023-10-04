df75d9b87105d62265e5fa167f5b0d0b35efd818
seylu <98249191+seyLu@users.noreply.github.com>
2023-10-04 23:14:53 +0800

    feat: add ProvinceQueryView and change methods to post

--- regions/urls.py
+++ regions/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path("districts/", views.DistrictsQueryView.as_view(), name="districts-query"),
     path("cities/", views.CitiesQueryView.as_view(), name="cities-query"),
+    path("provinces/", views.ProvincesQueryView.as_view(), name="provinces-query"),
 ]
--- regions/views.py
+++ regions/views.py
@@ -3,15 +3,15 @@ from urllib.parse import parse_qs, urlparse
 from django.http import HttpRequest, HttpResponse
 from django.views.generic import View
 
-from .models import City, District
+from .models import City, District, Province
 
 
 class DistrictsQueryView(View):
-    def get(self, request: HttpRequest) -> HttpResponse:
+    def post(self, request: HttpRequest) -> HttpResponse:
         parsed_url = urlparse(request.get_full_path())
         regions: dict[str, list[str]] = parse_qs(parsed_url.query)
 
-        response = None
+        response = []
 
         if code := regions.get("code"):
             code = code[0]  # type: ignore
@@ -21,22 +21,15 @@ class DistrictsQueryView(View):
             city_code = city_code[0]  # type: ignore
             response = District.objects.filter(city_code=city_code)  # type: ignore
 
-        elif province_code := regions.get("province_code"):
-            province_code = province_code[0]  # type: ignore
-            response = District.objects.filter(province_code=province_code)[:10]  # type: ignore
-
-        else:
-            response = District.objects.all()[:10]  # type: ignore
-
         return HttpResponse(response)
 
 
 class CitiesQueryView(View):
-    def get(self, request: HttpRequest) -> HttpResponse:
+    def post(self, request: HttpRequest) -> HttpResponse:
         parsed_url = urlparse(request.get_full_path())
         regions: dict[str, list[str]] = parse_qs(parsed_url.query)
 
-        response = None
+        response = []
 
         if code := regions.get("code"):
             code = code[0]  # type: ignore
@@ -46,7 +39,21 @@ class CitiesQueryView(View):
             province_code = province_code[0]  # type: ignore
             response = City.objects.filter(province_code=province_code)  # type: ignore
 
+        return HttpResponse(response)
+
+
+class ProvincesQueryView(View):
+    def post(self, request: HttpRequest) -> HttpResponse:
+        parsed_url = urlparse(request.get_full_path())
+        regions: dict[str, list[str]] = parse_qs(parsed_url.query)
+
+        response = []
+
+        if code := regions.get("code"):
+            code = code[0]  # type: ignore
+            response = Province.objects.get(code=code)
+
         else:
-            response = City.objects.all()[:10]  # type: ignore
+            response = Province.objects.all()
 
         return HttpResponse(response)
