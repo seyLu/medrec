6503f6010cea946bc2734bc2e338bf27803a5afb
seylu <98249191+seyLu@users.noreply.github.com>
2023-10-06 12:24:16 +0800

    ci: move mypy --ignore-missing-imports flag to pyproject.toml

--- clients/admin.py
+++ clients/admin.py
@@ -8,5 +8,5 @@ class ClientAdmin(admin.ModelAdmin[Client]):
     list_display = ("reference_number", "name", "type")
 
     @admin.display(description="name")
-    def name(self, obj: Client):
+    def name(self, obj: Client) -> str:
         return obj.full_name
