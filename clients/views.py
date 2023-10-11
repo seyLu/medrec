9fa1a126c34809bcfc96de86e2288f056c1262b2
seylu <98249191+seyLu@users.noreply.github.com>
2023-10-11 13:16:03 +0800

    WIP

--- clients/views.py
+++ clients/views.py
@@ -1,12 +1,27 @@
-from django.http import HttpRequest, HttpResponse
+from django.http import HttpRequest, HttpResponse, HttpResponseRedirect
+from django.shortcuts import render
+from django.urls import reverse
 from django.views.generic import View
+
+from records.models import Record
+
+from .models import Client
 
 
 class ClientCreateView(View):
     def post(self, request: HttpRequest) -> HttpResponse:
-        return HttpResponse("hello world")
+        reference_number: int = int(request.POST["reference_number"])
+        return HttpResponseRedirect(
+            reverse("client-detail", kwargs={"reference_number": reference_number})
+        )
 
 
 class ClientDetailView(View):
-    def post(self, request: HttpRequest, reference_number: int = 0) -> HttpResponse:
-        return HttpResponse()
+    def get(self, request: HttpRequest, reference_number: int = 0) -> HttpResponse:
+        client: Client = Client.objects.get(reference_number=reference_number)
+        records: Record = Record.objects.filter(client=client)
+        return render(
+            request,
+            "medrec/partials/client.html",
+            {"client": client, "records": list(records)},
+        )
