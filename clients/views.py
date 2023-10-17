d87a8b9f509aa9edd71693fffd2eab2695a022c1
セイル · MJ Sabit <98249191+seyLu@users.noreply.github.com>
2023-10-18 03:15:15 +0800

    [MEDREC-4] Update regions form to use HTMX + datalist  (#17)
    * feat: redo regions form using htmx and datalist
    
    * feat: reset modal state on toggle isEmployee
    
    * feat: add street_address on client form
    
    * feat: show client record on search
    
    * bugfix: fix mypy errors
    
    * feat: close modal on successful form submission
    
    * chore: sort tailwindcss classes

--- clients/views.py
+++ clients/views.py
@@ -1,12 +1,44 @@
-from django.http import HttpRequest, HttpResponse
+from typing import Any
+from urllib.parse import parse_qs, urlencode, urlparse
+
+from django.http import HttpRequest, HttpResponse, HttpResponseRedirect
+from django.shortcuts import redirect, render
+from django.urls import reverse
 from django.views.generic import View
+
+from records.models import Record
+
+from .models import Client
 
 
 class ClientCreateView(View):
-    def post(self, request: HttpRequest) -> HttpResponse:
-        return HttpResponse("hello world")
+    def post(self, request: HttpRequest) -> HttpResponseRedirect:
+        reference_number: int = int(request.POST["reference_number"])
+        response: str = reverse(
+            "client-detail",
+            kwargs={"reference_number": reference_number},
+        )
+        query_string: str = urlencode({"close-modal": True})
+        response += f"?{query_string}"
+
+        return redirect(response)
 
 
 class ClientDetailView(View):
-    def post(self, request: HttpRequest, reference_number: int = 0) -> HttpResponse:
-        return HttpResponse()
+    def get(self, request: HttpRequest, reference_number: int = 0) -> HttpResponse:
+        client: Client = Client.objects.get(reference_number=reference_number)
+        records: list[Record] = list(Record.objects.filter(client=client))
+
+        context: dict[str, Any] = {"client": client, "records": records}
+
+        parsed_url = urlparse(request.get_full_path())
+        query: dict[str, list[str]] = parse_qs(parsed_url.query)
+
+        if close_modal := query.get("close-modal"):
+            context["close_modal"] = close_modal
+
+        return render(
+            request,
+            "medrec/partials/client.html",
+            context,
+        )
--- regions/views.py
+++ regions/views.py
@@ -1,14 +1,15 @@
 from typing import Any
 from urllib.parse import parse_qs, urlparse
 
-from django.http import HttpRequest, JsonResponse
+from django.http import HttpRequest, HttpResponse, JsonResponse
+from django.shortcuts import render
 from django.views.generic import View
 
 from .models import City, District, Province
 
 
 class DistrictsQueryView(View):
-    def post(self, request: HttpRequest) -> JsonResponse:
+    def post(self, request: HttpRequest) -> JsonResponse | HttpResponse:
         parsed_url = urlparse(request.get_full_path())
         regions: dict[str, list[str]] = parse_qs(parsed_url.query)
 
@@ -22,14 +23,24 @@ class DistrictsQueryView(View):
             city = city[0]  # type: ignore
             response = District.objects.filter(city=city)  # type: ignore[misc]
 
-        if not response:
-            return JsonResponse(list(response), safe=False)
+        if not request.htmx:  # type: ignore[attr-defined]
+            if not response:
+                return JsonResponse(list(response), safe=False)
 
-        return JsonResponse(list(response.values()), safe=False)
+            return JsonResponse(list(response.values()), safe=False)
+
+        if not response:
+            return HttpResponse()
+
+        return render(
+            request,
+            "medrec/partials/regions-form/district-datalist.html",
+            {"districts": list(response)},
+        )
 
 
 class CitiesQueryView(View):
-    def post(self, request: HttpRequest) -> JsonResponse:
+    def post(self, request: HttpRequest) -> JsonResponse | HttpResponse:
         parsed_url = urlparse(request.get_full_path())
         regions: dict[str, list[str]] = parse_qs(parsed_url.query)
 
@@ -43,14 +54,24 @@ class CitiesQueryView(View):
             province = province[0]  # type: ignore
             response = City.objects.filter(province=province)  # type: ignore[misc]
 
-        if not response:
-            return JsonResponse(list(response), safe=False)
+        if not request.htmx:  # type: ignore[attr-defined]
+            if not response:
+                return JsonResponse(list(response), safe=False)
 
-        return JsonResponse(list(response.values()), safe=False)
+            return JsonResponse(list(response.values()), safe=False)
+
+        if not response:
+            return HttpResponse()
+
+        return render(
+            request,
+            "medrec/partials/regions-form/city-datalist.html",
+            {"cities": list(response)},
+        )
 
 
 class ProvincesQueryView(View):
-    def post(self, request: HttpRequest) -> JsonResponse:
+    def post(self, request: HttpRequest) -> JsonResponse | HttpResponse:
         parsed_url = urlparse(request.get_full_path())
         regions: dict[str, list[str]] = parse_qs(parsed_url.query)
 
@@ -63,7 +84,17 @@ class ProvincesQueryView(View):
         else:
             response = Province.objects.all()
 
-        if not response:
-            return JsonResponse(list(response), safe=False)
+        if not request.htmx:  # type: ignore[attr-defined]
+            if not response:
+                return JsonResponse(list(response), safe=False)
 
-        return JsonResponse(list(response.values()), safe=False)
+            return JsonResponse(list(response.values()), safe=False)
+
+        if not response:
+            return HttpResponse()
+
+        return render(
+            request,
+            "medrec/partials/regions-form/province-datalist.html",
+            {"provinces": list(response)},
+        )
