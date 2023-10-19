f8768f419f8df8dfc8b800e38e93f44c567d7256
seylu <98249191+seyLu@users.noreply.github.com>
2023-10-20 00:57:53 +0800

    bugfix: fix mypy errors

--- users/managers.py
+++ users/managers.py
@@ -38,9 +38,9 @@ class UserManager(BaseUserManager[AbstractUser]):
         extra_fields.setdefault("is_staff", True)  # type: ignore[arg-type]
         extra_fields.setdefault("is_superuser", True)  # type: ignore[arg-type]
 
-        if extra_fields["is_staff"] is not True:
+        if extra_fields.get("is_staff") is not True:  # type: ignore[comparison-overlap]
             raise ValueError(_("Superuser must have is_staff=True."))
-        if extra_fields["is_superuser"] is not True:
+        if extra_fields.get("is_superuser") is not True:  # type: ignore[comparison-overlap]
             raise ValueError(_("Superuser must have is_superuser=True."))
 
         return self.create_user(email, password, **extra_fields)
