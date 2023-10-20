77bd7b5e261691d6e1362b0dee4acb781b7ea881
セイル · MJ Sabit <98249191+seyLu@users.noreply.github.com>
2023-10-21 03:15:45 +0800

    [PROD-3] Debug render deploy (#26)
    * test: temporarily set debug to true
    
    * test: add django-tailwind build step
    
    * test: add django-tailwind install step
    
    * Revert "test: temporarily set debug to true"

--- build.sh
+++ build.sh
@@ -4,6 +4,9 @@ set -o errexit
 
 pip install -r requirements.txt
 
+python manage.py tailwind install
+python manage.py tailwind build
+
 python manage.py collectstatic --no-input
 python manage.py migrate
 
