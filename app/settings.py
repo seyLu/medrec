31b06fec13371a8bf5a3efc05f33587a768d7f0c
seylu <98249191+seyLu@users.noreply.github.com>
2023-10-21 01:45:44 +0800

    config: prep secret key for prod

--- app/settings.py
+++ app/settings.py
@@ -23,7 +23,10 @@ django_stubs_ext.monkeypatch()
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-=*(%1v&2znr5rwj%^y$trovb=$)sxgr^5j2ojnbn#jeyo#bx&1"
+SECRET_KEY = os.environ.get(
+    "SECRET_KEY",
+    default="django-insecure-=*(%1v&2znr5rwj%^y$trovb=$)sxgr^5j2ojnbn#jeyo#bx&1",
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
