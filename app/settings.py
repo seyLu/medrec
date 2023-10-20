cb0a48111e00c7443e60c6541e6d68f2d66eac19
seylu <98249191+seyLu@users.noreply.github.com>
2023-10-21 01:47:38 +0800

    config: prep debug for prod

--- app/settings.py
+++ app/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get(
 )
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = "RENDER" not in os.environ
 
 ALLOWED_HOSTS: list[str] = []
 
