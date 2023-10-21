763bb3afb1a76b297e86eb707b79cc1b06955444
seylu <98249191+seyLu@users.noreply.github.com>
2023-10-21 11:37:43 +0800

    feat: add test view

--- medrec/urls.py
+++ medrec/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path("", views.IndexView.as_view(), name="index"),
+    path("test/", views.TestView.as_view(), name="test"),
 ]
--- medrec/views.py
+++ medrec/views.py
@@ -6,3 +6,8 @@ from django.views.generic import View
 class IndexView(View):
     def get(self, request: HttpRequest) -> HttpResponse:
         return render(request, "medrec/index.html")
+
+
+class TestView(View):
+    def get(self, request: HttpRequest) -> HttpResponse:
+        return render(request, "medrec/test_index.html")
