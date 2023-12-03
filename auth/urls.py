cc0e23fff2e620b8d51d5333d48063b2ee8f677d
seylu <98249191+seyLu@users.noreply.github.com>
2023-12-04 01:26:38 +0800

    feat: revamp nav (WIP)

--- auth/urls.py
+++ auth/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path("login/", views.LoginView.as_view(), name="login"),
     path("logout/", views.LogoutView.as_view(), name="logout"),
     path("register/", views.RegisterView.as_view(), name="register"),
+    path("demo/", views.DemoView.as_view(), name="demo"),
 ]
--- medrec/views.py
+++ medrec/views.py
@@ -1,6 +1,9 @@
 from django.http import HttpRequest, HttpResponse
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.urls import reverse
 from django.views.generic import View
+
+from users.models import User
 
 
 class IndexView(View):
@@ -10,4 +13,21 @@ class IndexView(View):
 
 class TestView(View):
     def get(self, request: HttpRequest) -> HttpResponse:
-        return render(request, "medrec/test_index.html")
+        if not request.user.is_authenticated:
+            return redirect(reverse("login"))
+
+        user: User = User.objects.get(email=request.user)  # type: ignore[assignment]
+        email: str = user.email
+        avatar: str = ""
+        display_name: str = ""
+
+        avatar = email[0].upper()
+        display_name = email.split("@")[0].title()
+
+        context = {
+            "email": email,
+            "avatar": avatar,
+            "display_name": display_name,
+        }
+
+        return render(request, "medrec/test_index.html", context)
